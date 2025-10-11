06f0c7d9a1e3cee00839b3e134376ad751d65bdb
sudhir-rajai <rajaisudhir11@gmail.com>
2025-10-11 16:31:41 +0530

    small fixes

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="py-6 px-6 text-center">
-        <p class="mb-0 fs-4">Design and Developed by <a href="" target="_blank"
+        <p class="mb-0 fs-4">Design and Developed by <a href="https://sudhirinsights.com" target="_blank"
                 class="pe-1 text-primary text-decoration-none">Sudhir</a></p>
     </div>
 </template>
