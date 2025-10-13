5a0c8045bdc5ff1c9047d38ccb637fca82d37329
sudhir-rajai <rajaisudhir11@gmail.com>
2025-10-13 23:17:36 +0530

    small fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" type="image/png" href="/assets/images/logos/larasafe-favicon.png" />
     <link rel="stylesheet" href="/assets/css/styles.min.css" />
     @routes
