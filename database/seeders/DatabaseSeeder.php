8aed8c3127f92a34f2b8c4d2266f38e57d613f4d
sudhir-rajai <rajaisudhir11@gmail.com>
2025-10-13 22:34:37 +0530

    small fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'LaraSafe',
-            'email' => 'user@example.com',
-            'password' => '12345678',
+               'id' => (string) Str::uuid(),
+                'name' => 'LaraSafe',
+                'email' => 'sudhirrajai@proton.me',
+                'email_verified_at' => now(),
+                'password' => bcrypt('12345678'),
         ]);
     }
 }
